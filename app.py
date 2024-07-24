0aa42ba6fdcdaeca4b6063b7cb5770c51fae554f
paulruizocasystem <paul.ruiz@ocasystem.com>
2024-07-24 18:55:49 -0500

    documentando el código

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import time
-import pandas as pd
+from selenium import webdriver #Importar el módulo de WebDriver para controlar el navegador
+from selenium.webdriver.chrome.service import Service #Importar el módulo de servicio para especificar la ruta del chromedriver
+from selenium.webdriver.support.ui import WebDriverWait #Importar WebDriverWait para manejar esperas explícitas
+from selenium.webdriver.support import expected_conditions as EC #Importar condiciones esperadas para las esperas
+from selenium.webdriver.common.by import By # Importar By para seleccionar elementos por
+import time #Importar time para usar sleep y pausar la ejecución
+import pandas as pd #Importar pandas, útil para manipular datos
 
-#Opciones de navegación:
+#configuración de opciones para el navegador
 options = webdriver.ChromeOptions()
-options.add_argument('--start-maximized')
-options.add_argument('--disable-extensions')
+options.add_argument('--start-maximized') #Maximizar la ventana del navegador
+options.add_argument('--disable-extensions') #Deshabilitar extensiones
 
-#Ruta del driver
+#la ruta del driver
 driver_path = r'C:\SeleniumDrivers\chromedriver.exe'  
 service = Service(driver_path)
 
-#Inicializar el navegador
+#Inicializa el navegador con las opciones configuradas
 driver = webdriver.Chrome(service=service, options=options)
 
-#Navegar a la URL
+#Acceder a la URL específica
 driver.get('https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp')
 
-
-#rellenar en el campo para el ruc:
+#Esperar hasta que el campo de RUC sea clicable y enviar el RUC
 WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, 'input#txtRuc'))
-    ).send_keys('20612534242')
+).send_keys('20612534242')
 
-#hacer click:
+# Esperar hasta que el botón de consulta sea clicable y hacer clic
 WebDriverWait(driver, 5).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn btn-primary'.replace(' ', '.')))
-    ).click()
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-primary'))
+).click()
 
-time.sleep(10)
+# Pausar la ejecución para observar los resultados
+time.sleep(100)
+
+#Cerrar el navegador
 driver.quit()
