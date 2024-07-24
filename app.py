4b34215d2b432a35110f2d2b31283dedf219411a
paulruizocasystem <paul.ruiz@ocasystem.com>
2024-07-24 18:23:12 -0500

    buscador en sunat activo con el ruc de ocasystem

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
 
-driver = webdriver.Chrome()
+#Opciones de navegación:
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+options.add_argument('--disable-extensions')
 
-driver.get("http://selenium.dev")
+#Ruta del driver
+driver_path = r'C:\SeleniumDrivers\chromedriver.exe'  
+service = Service(driver_path)
 
+#Inicializar el navegador
+driver = webdriver.Chrome(service=service, options=options)
+
+#Navegar a la URL
+driver.get('https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp')
+
+
+#rellenar en el campo para el ruc:
+WebDriverWait(driver, 5).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'input#txtRuc'))
+    ).send_keys('20612534242')
+
+#hacer click:
+WebDriverWait(driver, 5).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn btn-primary'.replace(' ', '.')))
+    ).click()
+
+time.sleep(10)
 driver.quit()
