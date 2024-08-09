68ad929a96532aa590a1e524825398ab92e36dea
paulruizocasystem <paul.ruiz@ocasystem.com>
2024-08-08 22:11:19 -0500

    actualizacion de codigo

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
-from selenium import webdriver #Importar el módulo de WebDriver para controlar el navegador
-from selenium.webdriver.chrome.service import Service #Importar el módulo de servicio para especificar la ruta del chromedriver
-from selenium.webdriver.support.ui import WebDriverWait, Select #Importar WebDriverWait para manejar esperas explícitas
-from selenium.webdriver.support import expected_conditions as EC #Importar condiciones esperadas para las esperas
-from selenium.webdriver.common.by import By # Importar By para seleccionar elementos por
-from selenium.common.exceptions import NoSuchElementException
-import time #Importar time para usar sleep y pausar la ejecución
-import pandas as pd #Importar pandas, útil para manipular datos
+from time import sleep  # Importa la función sleep para pausas
+from selenium import webdriver  # Importa Selenium para control del navegador
+from selenium.webdriver.common.by import By  # Importa By para localización de elementos
+from selenium.webdriver.chrome.options import Options # Importa Options para configurar Chrome
+from selenium.webdriver.chrome.service import Service  # Importa Service para manejar el servicio de Chrome
+from selenium.webdriver.support.ui import WebDriverWait, Select  # Importa WebDriverWait y Select para manejar esperas explícitas y listas desplegables
+from selenium.webdriver.support import expected_conditions as EC  # Importa condiciones esperadas para las esperas
+from selenium.common.exceptions import NoSuchElementException  # Importa las excepciones de Selenium
 
-#configuración de opciones para el navegador
-options = webdriver.ChromeOptions()
-options.add_argument('--start-maximized') #Maximizar la ventana del navegador
-options.add_argument('--disable-extensions') #Deshabilitar extensiones
+from webdriver_manager.chrome import ChromeDriverManager  # Importa ChromeDriverManager para manejar el driver de Chrome
 
-#la ruta del driver
-driver_path = r'C:\SeleniumDrivers\chromedriver.exe'  
-service = Service(driver_path)
+opts = Options()  # Crea una instancia de opciones de Chrome
 
-#Inicializa el navegador con las opciones configuradas
-driver = webdriver.Chrome(service=service, options=options)
+# Configuración de user agent:
+opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")  # Añade un user agent específico
+opts.add_argument("--headless") # Añade la opción headless para no abrir la ventana del navegador
 
-#Acceder a la URL específica
-driver.get('https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp')
+# Deshabilitar los logs de la consola de DevTools
+opts.add_argument("--log-level=3")
 
-#funcion para buscar por ruc:
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),  # Instala y usa el driver de Chrome
+    options=opts  # Aplica las opciones configuradas
+)
+
+driver.get('https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp')  # Accede a la URL de SUNAT
+
+sleep(3)  # Espera 3 segundos para asegurar que la página cargue
+
+# Función para buscar por RUC
 def buscar_por_ruc(ruc):
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnPorRuc"))).click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "txtRuc"))).send_keys(ruc)
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnAceptar"))).click()
+    
     # Esperar a que los elementos de la lista sean visibles y almacenarlos en una lista
     items = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".list-group-item")))
     
@@ -36,14 +42,14 @@ def buscar_por_ruc(ruc):
             heading = item.find_element(By.CSS_SELECTOR, "h4.list-group-item-heading").text
             text = item.find_element(By.CSS_SELECTOR, "p.list-group-item-text").text
             print(f"Encabezado: {heading}, Texto: {text}")
-        except Exception as e:
+        except NoSuchElementException as e:
             print(f"Error al extraer información de un elemento: {e}")
 
-#funcion para buscar por documento:
+# Función para buscar por documento
 def buscar_por_documento(tipo_documento, numero_documento):
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnPorDocumento"))).click()
     select = Select(WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "cmbTipoDoc"))))
-
+    
     if tipo_documento.lower() == 'documento nacional de identidad':
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "txtNumeroDocumento"))).send_keys(numero_documento)
     elif tipo_documento.lower() == 'carnet de extranjeria':
@@ -55,13 +61,12 @@ def buscar_por_documento(tipo_documento, numero_documento):
     elif tipo_documento.lower() == 'cedula diplomatica de identidad':
         select.select_by_value('A')  # Valor para Cedula Diplomática de Identidad
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "txtNumeroDocumento"))).send_keys(numero_documento)
-
+    
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnAceptar"))).click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.list-group-item.clearfix.aRucs"))).click()
+    
     # Esperar a que los elementos de la lista sean visibles
-    items = WebDriverWait(driver, 10).until(
-        EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".list-group-item"))
-    )
+    items = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".list-group-item")))
     
     # Extraer y mostrar encabezados y textos utilizando XPath
     for i, item in enumerate(items, start=1):
@@ -71,40 +76,40 @@ def buscar_por_documento(tipo_documento, numero_documento):
             print(f"Item {i}: Encabezado - {heading}, Texto - {text}")
         except NoSuchElementException:
             print(f"Item {i}: No se encontró el elemento esperado.")
-#funcion para buscar por nombre o razon social:
+
+# Función para buscar por nombre o razón social
 def buscar_por_nombre_razon_social(nombre_razon):
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnPorRazonSocial"))).click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "txtNombreRazonSocial"))).send_keys(nombre_razon)
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnAceptar"))).click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.list-group-item.clearfix.aRucs"))).click()
-      # Extracción de datos con XPath
+    
+    # Extracción de datos con XPath
     text_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='list-group-item'][1]/div/div[2]/h4")))
     ruc_text = text_element.text
     print("Número de RUC y Nombre:", ruc_text)
-
+    
     tipo_contribuyente_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='list-group-item'][2]/div/div[2]/p")))
     tipo_contribuyente_text = tipo_contribuyente_element.text
     print("Tipo Contribuyente:", tipo_contribuyente_text)
 
-
 # Ejemplo de uso
-buscar_por_documento('documento nacional de identidad', '45282085')
-time.sleep(10)  # Tiempo para observar los resultados
+buscar_por_documento('documento nacional de identidad', '46878365')
+sleep(10)  # Tiempo para observar los resultados
 
 """# Ejemplo de uso
 buscar_por_ruc('20605431543')
-time.sleep(10)  # Tiempo para observar los resultados"""
+sleep(10)  # Tiempo para observar los resultados
 
-"""#Ejemplo de uso
+# Ejemplo de uso
 buscar_por_nombre_razon_social('TIENDAS POR DEPARTAMENTO RIPLEY S.A.C.')
-time.sleep(10)  # Tiempo para observar los resultados"""
+sleep(10)  # Tiempo para observar los resultados
 
-"""#Ejemplo de uso
+# Ejemplo de uso
 buscar_por_documento('Carnet de Extranjeria', '001077238')
-time.sleep(10)  # Tiempo para observar los resultados"""
-
+sleep(10)  # Tiempo para observar los resultados
+"""
 # Cerrar el navegador
 driver.quit()
-
 
 
